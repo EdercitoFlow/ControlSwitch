2ce133c5cbb24a3cebfc0e6b62050eceed45d381
Eder <20253ds113@utez.edu.mx>
2026-01-27 22:36:55 -0600

    Correcion de Errores de ejecución

--- ControlSwitchEderGarcia/src/Main.java
+++ ControlSwitchEderGarcia/src/Main.java
@@ -7,12 +7,14 @@ public class Main {
     public static double resultado=0;
     public static void main(String[] args) {
     Scanner scanner = new Scanner (System.in);
-        System.out.println("Bienvenido al menú con las siguientes opciones: \n 1) Sumar\n 2) Restar\n 3) Multiplicar\n 4) Dividir");
-        Opcion=scanner.nextInt();
+        System.out.println("Bienvenido al menú, para empezar:");
         System.out.println("Digita el primer numero: ");
         numeroUno=scanner.nextDouble();
         System.out.println("Digita el segundo numero: ");
         numeroDos=scanner.nextDouble();
+        System.out.println("Este menú cuenta con las siguientes opciones: \n 1) Sumar\n 2) Restar\n 3) Multiplicar\n 4) Dividir");
+        Opcion=scanner.nextInt();
+
         switch(Opcion){
             case 1:
                 System.out.println("Seleccionaste Suma");
